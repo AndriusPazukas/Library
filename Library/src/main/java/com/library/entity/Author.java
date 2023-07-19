e1236f4a8a2b93f72e676c4528c7089058a14fad
Andrius <andriuspazukas1981@gmai.com>
2023-07-19 14:22:18 +0000

    Initial commit

---
+++ Library/src/main/java/com/library/entity/Author.java
@@ -0,0 +1,66 @@
+package com.library.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(name = "name")
+    private String authorName;
+    @Column(name = "date_of_birth")
+    private String dateOfBirth;
+    @Column
+    private String nationality;
+    @OneToMany(mappedBy = "author", fetch = FetchType.EAGER)
+    List<Book> books;
+
+
+    public Author() {
+    }
+
+    public Author(String authorName, String dateOfBirth, String nationality) {
+        super();
+        this.authorName = authorName;
+        this.dateOfBirth = dateOfBirth;
+        this.nationality = nationality;
+
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
---
+++ Library/src/main/java/com/library/entity/Book.java
@@ -0,0 +1,70 @@
+package com.library.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+    @Column(name = "book_name")
+    private String name;
+    @Column(name = "author")
+    @ManyToOne
+    @JoinColumn
+    private Author author;
+    @Column(name = "price")
+    private int price;
+    @Column(name = "quantity")
+    private int quantity;
+
+    public Book(String name, Author author, int price, int quantity){
+        super();
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
